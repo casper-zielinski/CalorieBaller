a8dd8f86357c74d9fe9fe8241221e1d3e912f3f2
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-03 11:45:03 +0100

    Initial Commit

---
+++ CalorieBeller/CalorieBellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieBellerApp.swift
+//  CalorieBeller
+//
+//  Created by Zielinski Casper on 03.03.26.
+//
+
+import SwiftUI
+
+@main
+struct CalorieBellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieBeller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalorieBeller
+//
+//  Created by Zielinski Casper on 03.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
